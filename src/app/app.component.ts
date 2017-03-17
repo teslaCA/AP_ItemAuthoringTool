96e9b1cf9037c4d6ad3439422731e7ac684e6975
Brett Drainer <bdrainer@fairwaytech.com>
2017-03-16 21:16:50 -0700

    IAT-216:  Upgraded to Bootstrap 4.  Switched from Less to Sass (.scss files).  Added a navbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,13 @@
 import { Component } from '@angular/core';
-import {Http, Response} from '@angular/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
-  singleModel = '1';
 
-  data: Object;
-  loading: boolean;
-
-  constructor(private http: Http) {
-  }
-
-  makeRequest(): void {
-
-    this.loading = true;
-
-    this.http.request('/api/get')
-      .subscribe((res: Response) => {
-        this.data = res.text();
-        this.loading = false;
-      });
-  }
+  title = 'App works!';
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }  from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
+import { RouterModule }   from '@angular/router';
+import { HomeModule }     from './home';
+import { NavbarModule }   from './navbar';
+
 import { AlertModule, ButtonsModule, TabsModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
@@ -14,9 +18,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    HomeModule,
+    NavbarModule,
     AlertModule.forRoot(),
     ButtonsModule.forRoot(),
-    TabsModule.forRoot()
+    TabsModule.forRoot(),
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
