6a1eb4c281352cf6be8dca2eef919179fb73d328
Ray Mitchell <ray@raymondmitchell.com>
2017-06-13 16:32:49 -0700

    IAT-440: Replace “create item component” with busy message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,14 +12,12 @@ import {HeaderComponent} from "./header.component";
 import {FooterComponent} from "./footer.component";
 import {CreateItemSelectTypeComponent} from "./item/components/create-item-select-type/create-item-select-type.component";
 import {ItemDashboardComponent} from "./item/components/item-dashboard/item-dashboard.component";
-import {CreateItemComponent} from "./item/components/create-item/create-item.component";
 import {LoadSaItemComponent} from "./item/components/load-sa-item/load-sa-item.component";
 import {ToastyModule} from "ng2-toasty";
 import {FindItemComponent} from "./item/components/find-item/find-item.component";
 import {Logger} from "./core/logger.service";
 import {UserService} from "./core/user.service";
 import {BuildInfoService} from "./core/build-info.service";
-import {BusyOverlayComponent} from "./core/busy.service/busy-overlay.component";
 import {CoreModule} from "./core/core.module";
 
 describe('AppComponent', () => {
@@ -34,7 +32,6 @@ describe('AppComponent', () => {
         FooterComponent,
         ItemDashboardComponent,
         CreateItemSelectTypeComponent,
-        CreateItemComponent,
         LoadSaItemComponent,
         FindItemComponent
       ],
--- src/app/item/components/create-item-select-type/create-item-select-type.component.ts
+++ src/app/item/components/create-item-select-type/create-item-select-type.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from "@angular/core";
 import {ItemType} from "../../models/item-type";
 import {ItemTypeService} from "../../services/item-type.service";
+import {ItemService} from "../../services/item.service";
+import {Logger} from "../../../core/logger.service";
+import {Router} from "@angular/router";
+import {AlertService} from "../../../core/alert.service";
+import {BusyService} from "../../../core/busy.service/busy.service";
 
 @Component({
   selector: 'create-item-select-type',
@@ -10,10 +15,33 @@ import {ItemTypeService} from "../../services/item-type.service";
 export class CreateItemSelectTypeComponent implements OnInit {
   public normalItemTypes: ItemType[];
 
-  constructor(private itemTypeService: ItemTypeService) {
+  constructor(private logger: Logger,
+              private router: Router,
+              private alertService: AlertService,
+              private busyService: BusyService,
+              private itemService: ItemService,
+              private itemTypeService: ItemTypeService) {
   }
 
   ngOnInit() {
     this.normalItemTypes = this.itemTypeService.findNormalItemTypes();
+  }
+
+  createItem(itemTypeCode: string) {
+    this.logger.debug(`Creating item of type ${itemTypeCode}`);
+    this.busyService.show("Creating Item");
+    this.itemService.beginItemCreate('asdf')
+      .subscribe(
+        item => {
+          this.logger.debug(`Successfully created item ${JSON.stringify(item)}`);
+          this.busyService.hide();
+          this.router.navigateByUrl(`/item/${item.id}`);
+        },
+        error => {
+          this.logger.error(`Failed to create item of type ${itemTypeCode}, error ${JSON.stringify(error)}`);
+          this.busyService.hide();
+          this.alertService.error("Error Creating Item", `An error was encountered while creating your item`);
+        }
+      );
   }
 }
