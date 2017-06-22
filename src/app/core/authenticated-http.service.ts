8c3f2f60e71ea12c62b3a8cd0a3d8f32994bd3e1
Brett Drainer <brett@gearlaunch.com>
2017-06-22 13:21:43 -0700

    IAT-423:  Added comments to the AuthenticatedHttpService.

--- src/app/core/authenticated-http.service.ts
+++ src/app/core/authenticated-http.service.ts
@@ -5,6 +5,16 @@ import {Logger} from "./logger.service";
 import {AlertService} from "./alert.service";
 import {BusyService} from "./busy.service/busy.service";
 
+/**
+ * Extends Http and intercepts all request calls.  The purpose is to
+ * check for response error 401 Unauthorized.  The backend is using SAML.
+ * When the frontend calls the bankend where the session has timed out the
+ * SAML redirect flows start taking place ultimately resulting in the
+ * frontend failing because it can't parse what the IDP gives back
+ * which happens to be a self submitting HTML page.  The fix is to catch
+ * the 401 error and refresh the page.  This avoids the SAML redirects
+ * and gives the frontend the ability to react the way it wants to.
+ */
 @Injectable()
 export class AuthenticatedHttpService extends Http {
 
