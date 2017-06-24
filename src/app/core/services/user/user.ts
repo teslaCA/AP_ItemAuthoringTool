aa5f871692ce8b49f6e689b13ae4fb1d5599aa2f
Ray Mitchell <ray@raymondmitchell.com>
2017-06-24 04:18:44 -0700

    IAT-480: Clarify comment.

--- src/app/core/services/user/user.ts
+++ src/app/core/services/user/user.ts
@@ -3,7 +3,7 @@ import {JsonObject, JsonProperty} from "json2typescript";
 /**
  * User model that can be mapped to/from JSON.
  *
- * Important: Fields must be initialized to undefined to take part in mapping.
+ * Important: Fields must be initialized to a value or undefined to take part in mapping.
  * See https://github.com/dhlab-basel/json2typescript for more info.
  */
 @JsonObject
