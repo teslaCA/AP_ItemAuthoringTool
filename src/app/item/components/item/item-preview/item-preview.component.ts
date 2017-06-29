48211f3e2daf88e7220dbde37d0685882ed0390b
Alex Ponce <aponce@recurly.com>
2017-06-28 20:16:26 -0700

    Completed proof of concept

--- src/app/item/components/item/item-preview/item-preview.component.ts
+++ src/app/item/components/item/item-preview/item-preview.component.ts
@@ -1,5 +1,6 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ModalDirective} from 'ngx-bootstrap';
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-item-preview',
@@ -7,17 +8,33 @@ import {ModalDirective} from 'ngx-bootstrap';
   styleUrls: ['./item-preview.component.less'],
 })
 export class ItemPreviewComponent implements OnInit {
+  loading = true;
+  itemId: string;
 
-  constructor() { }
+  constructor(private domSanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    console.log('Entered ItemPreviewComponent OnInit')
   }
+
 
   @ViewChild('previewModal') modal:ModalDirective;
 
   showModal(id: string) {
     console.log("item preview id:" + id);
+    this.itemId = id;
+
+    this.loading = false;
     this.modal.show();
+  }
+
+  getTrustedUrl() {
+    let sanitizedUrl;
+    let itemUrl = 'http://ivs-awsuat.sbtds.org/item/187-2061';
+    //if (this.renderResponse) {
+      sanitizedUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(itemUrl);
+    //}
+    return sanitizedUrl;
   }
 
 }
