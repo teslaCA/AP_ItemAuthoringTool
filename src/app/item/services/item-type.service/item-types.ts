46d0a3d72359052a1b4f650045c610b8321c67ef
Brett Drainer <brett@gearlaunch.com>
2017-08-01 09:27:35 -0700

    IAT-601: Fixed lint errors in item-types.ts

--- src/app/item/services/item-type.service/item-types.ts
+++ src/app/item/services/item-type.service/item-types.ts
@@ -1,115 +1,103 @@
 import {ItemType} from "./item-type";
 import {ItemTypeCategory} from "./item-type-category";
 
-export const EBSR: ItemType =
-  {
-    category: ItemTypeCategory.Item,
-    categoryName: 'Item',
-    icon: 'fa-list',
-    name: 'Evidence-Based Select Response',
-    code: 'EBSR'
-  };
+export const EBSR: ItemType = {
+  category: ItemTypeCategory.Item,
+  categoryName: 'Item',
+  icon: 'fa-list',
+  name: 'Evidence-Based Select Response',
+  code: 'EBSR'
+};
 
-// export const EQ: ItemType =
-//   {
+// export const EQ: ItemType = {
 //     category: ItemTypeCategory.Item,
 //     categoryName: 'Item',
 //     icon: 'fa-superscript',
 //     name: 'Equation',
 //     code: 'eq'
-//   };
+// };
 
-// export const GI: ItemType =
-//   {
+// export const GI: ItemType = {
 //     category: ItemTypeCategory.Item,
 //     categoryName: 'Item',
 //     icon: 'fa-bar-chart',
 //     name: 'Grid Item',
 //     code: 'gi'
-//   };
+// };
 
 
-// export const HT: ItemType =
-//   {
+// export const HT: ItemType = {
 //     category: ItemTypeCategory.Item,
 //     categoryName: 'Item',
 //     icon: 'fa-fire',
 //     name: 'Hot Text',
 //     code: 'ht'
-//   };
+// };
 
-export const MC: ItemType =
-  {
-    category: ItemTypeCategory.Item,
-    categoryName: 'Item',
-    icon: 'fa-list-ul',
-    name: 'Multiple Choice',
-    code: 'mc'
-  };
+export const MC: ItemType = {
+  category: ItemTypeCategory.Item,
+  categoryName: 'Item',
+  icon: 'fa-list-ul',
+  name: 'Multiple Choice',
+  code: 'mc'
+};
 
-// export const MI: ItemType =
-//   {
+// export const MI: ItemType = {
 //       category: ItemTypeCategory.Item,
 //       categoryName: 'Item',
 //       icon: 'fa-list-ul',
 //       name: 'Match Interaction',
 //       code: 'mi'
-//   };
+// };
 
-export const MS: ItemType =
-  {
-    category: ItemTypeCategory.Item,
-    categoryName: 'Item',
-    icon: 'fa-th-list',
-    name: 'Multiple Select',
-    code: 'ms'
-  };
+export const MS: ItemType = {
+  category: ItemTypeCategory.Item,
+  categoryName: 'Item',
+  icon: 'fa-th-list',
+  name: 'Multiple Select',
+  code: 'ms'
+};
 
-export const SA: ItemType =
-  {
-    category: ItemTypeCategory.Item,
-    categoryName: 'Item',
-    icon: 'fa-font',
-    name: 'Short Answer',
-    code: 'sa'
-  };
+export const SA: ItemType = {
+  category: ItemTypeCategory.Item,
+  categoryName: 'Item',
+  icon: 'fa-font',
+  name: 'Short Answer',
+  code: 'sa'
+};
 
 
-export const STIM: ItemType =
-  {
-    category: ItemTypeCategory.Stimulus,
-    categoryName: 'Stimulus',
-    icon: 'fa-commenting-o',
-    name: 'Stimulus',
-    code: 'stim'
-  };
+export const STIM: ItemType = {
+  category: ItemTypeCategory.Stimulus,
+  categoryName: 'Stimulus',
+  icon: 'fa-commenting-o',
+  name: 'Stimulus',
+  code: 'stim'
+};
 
-// export const TI: ItemType =
-//   {
+// export const TI: ItemType = {
 //       category: ItemTypeCategory.Item,
 //       categoryName: 'Item',
 //       icon: 'fa-table',
 //       name: 'Table Interaction',
 //       code: 'ti'
-//   };
+// };
 
-export const TUT: ItemType =
-  {
-    category: ItemTypeCategory.Tutorial,
-    categoryName: 'Tutorial',
-    icon: 'fa-question-circle',
-    name: 'Tutorial',
-    code: 'tut',
-  }
+export const TUT: ItemType = {
+  category: ItemTypeCategory.Tutorial,
+  categoryName: 'Tutorial',
+  icon: 'fa-question-circle',
+  name: 'Tutorial',
+  code: 'tut',
+};
 
-export const WER: ItemType =
-  {
-    category: ItemTypeCategory.Item,
-    categoryName: 'Item',
-    icon: 'fa-comments',
-    name: 'Writing Extended Response',
-    code: 'wer'
-  };
+export const WER: ItemType = {
+  category: ItemTypeCategory.Item,
+  categoryName: 'Item',
+  icon: 'fa-comments',
+  name: 'Writing Extended Response',
+  code: 'wer'
+};
 
 export const itemTypes: ItemType[] = [
   // TODO: Do not delete the commented-out item types; uncomment them as they are implemented
