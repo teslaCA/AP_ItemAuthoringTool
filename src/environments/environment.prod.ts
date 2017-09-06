ab0d1074e10c1888c3b1415f6d7bc05f8bbd1ea1
Alex Ponce <aponce@recurly.com>
2017-09-05 21:04:43 -0700

    Increased overal timeout to 1 hour; Increased session timeout warning to show 5 minutes prior to automatic logout

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   idleService: {
-    idleDuration: 540,          // Duration in seconds of maximum allowed of time a logged in session is allowed to remain idle
-    timeoutDuration: 60,        // Duration in seconds that an alert will be presented to the user giving them an option to remain logged in
+    idleDuration: 3300,         // Duration in seconds of maximum allowed idle time before an alert message is displayed
+    timeoutDuration: 300,       // Duration in seconds that an alert will be presented to the user giving them an option to remain logged in
     logOutAfterTimeout: true    // Log outs application after timeout. Set to false only in development environments
   }
 };
